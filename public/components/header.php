1c2a389ee62d2d068f4ae15a87c85f558f58c172
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-04-08 21:53:47 -0300

    adicionando footer

--- public/components/header.php
+++ public/components/header.php
@@ -4,7 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?=BASE_URL?>/public/assets/bootstrap-5.0.0/css/bootstrap.min.css">
+  <!-- Bootstrap 5.0.0 -->
+  <link rel="stylesheet" href="<?=BASE_URL?>/public/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?=BASE_URL?>/public/assets/css/styles.css">
   <title>Projeto Acessibilidade</title>
 </head>
 <body>
