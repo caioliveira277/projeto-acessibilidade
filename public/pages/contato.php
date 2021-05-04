1b9484edfa24f37cd2751de3491e4f326fae0de4
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-05-04 20:17:42 -0300

    adicionando dropdown contato

--- public/pages/contato.php
+++ public/pages/contato.php
@@ -14,13 +14,13 @@
     </div>
 
     <div class="row p-3">
-      <div class="col-12 bg-dark d-flex align-items-center contato_sobre">
-        <div class="col-6 text-white d-flex flex-column align-items-center justify-content-center contato_sobre">
+      <div class="col-12 bg-dark d-flex align-items-center">
+        <div class="col-6 text-white d-flex flex-column align-items-center justify-content-center">
           <p class="change-font-size">Email: acessilidade@gmail.com</p><br>
           <p class="change-font-size">Telefone: (11) 4141-4141</p>
 
         </div>
-        <div class="col-6 text-white d-flex flex-column align-items-center justify-content-center contato_sobre borda_sobre">
+        <div class="col-6 text-white d-flex flex-column align-items-center justify-content-center borda_sobre">
           <div class="p-2">
             <div class="d-flex align-self-start">
               <h2 class="text-white change-font-size">Sobre nós</h2>
@@ -40,25 +40,25 @@
     </div>
 
     <div class="row p-3">
-      <div class="col-12 bg-dark text-white d-flex p-3 contato_formulario">
+      <div class="col-12 bg-dark text-white d-flex p-3">
         <form class="col-9">
           <div class="form-group">
-            <label for="exampleFormControlInput1" class="change-font-size">Nome</label>
-            <input type="email" class="col-12" class="form-control" id="exampleFormControlInput1" placeholder="Digite seu Nome">
+            <label for="nome" class="change-font-size mb-2">Nome</label>
+            <input type="text" class="form-control" id="nome" placeholder="Digite seu Nome">
           </div>
           <div class="form-group mt-2">
-            <label for="exampleFormControlInput1" class="change-font-size">Telefone</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Digite seu telefone para contato">
+            <label for="telefone" class="change-font-size mb-2">Telefone</label>
+            <input type="tel" class="form-control" id="telefone" placeholder="Digite seu telefone para contato">
           </div>
           <div class="form-group mt-2">
-            <label for="exampleFormControlInput1" class="change-font-size">E-mail</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Escreva seu E-mail">
+            <label for="email" class="change-font-size mb-2">E-mail</label>
+            <input type="email" class="form-control" id="email"  placeholder="Escreva seu E-mail">
           </div>
           <div class="form-group mt-2">
-            <label for="exampleFormControlTextarea1" class="change-font-size">Dúvida</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Escreva seu sua dúvida" rows="3"></textarea>
+            <label for="duvida" class="change-font-size mb-2">Dúvida</label>
+            <textarea class="form-control" id="duvida" placeholder="Escreva seu sua dúvida" rows="3"></textarea>
           </div>
-          <div class="p-4">
+          <div class="ps-2 pt-4">
             <a href="#" class="btn text-center botao text-white change-font-size">ENVIAR</a>
           </div>
         </form>
@@ -79,7 +79,7 @@
           <h4 class="change-font-size">Número: (11) 99999-9999</h4>
         </div>
         <div class="col-6 d-flex align-items-center justify-content-center">
-          <a href="#" class="btn text-center botao text-white change-font-size">Abrir seu WhatsApp</a>
+          <a href="#" class="btn text-center botao text-white change-font-size">Abrir WhatsApp</a>
         </div>
       </div>
     </div>
@@ -92,24 +92,61 @@
     <div class="p-4">
       <div class="row">
         <div class="col">
-          <h3 class="cor_texto change-font-size">Linkedin</h3>
-          <p class="change-font-size">Alex Teixeira - <a class="sr-only sr-only-focusable" href="">Linkedin</a> </p>
-          <p class="change-font-size">Caio Oliveira - <a class="sr-only sr-only-focusable" href="">Linkedin</a></p>
-          <p class="change-font-size">Guilherme Moreira - <a class="sr-only sr-only-focusable" href="">Linkedin</a></p>
-          <p class="change-font-size">Leonardo Bozzi - <a class="sr-only sr-only-focusable" href="">Linkedin</a> </p>
-          <p class="change-font-size">Juarez Souza - <a class="sr-only sr-only-focusable" href="">Linkedin</a> </p>
+          <a data-bs-toggle="collapse" href="#collapseLinkedin" role="button" aria-expanded="false" aria-controls="collapseLinkedin">
+            <h3 class="cor_texto change-font-size">
+              Linkedin 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M4.854 14.854a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V3.5A2.5 2.5 0 0 1 6.5 1h8a.5.5 0 0 1 0 1h-8A1.5 1.5 0 0 0 5 3.5v9.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4z"/>
+              </svg>
+            </h3>
+          </a>
+          <div class="collapse" id="collapseLinkedin">
+              <p class="change-font-size">Alex Teixeira - <a class="text-primary" href=""><u>Linkedin</u></a> </p>
+              <p class="change-font-size">Caio Oliveira - <a class="text-primary" href=""><u>Linkedin</u></a></p>
+              <p class="change-font-size">Guilherme Moreira - <a class="text-primary" href=""><u>Linkedin</u></a></p>
+              <p class="change-font-size">Leonardo Bozzi - <a class="text-primary" href=""><u>Linkedin</u></a> </p>
+              <p class="change-font-size">Juarez Souza - <a class="text-primary" href=""><u>Linkedin</u></a> </p>
+          </div>
         </div>
       </div>
       <hr>
       <div class="row mt-3">
         <div class="col">
-          <h3 style="color:#8844EE" class="change-font-size">Github</h3>
+          <a data-bs-toggle="collapse" href="#collapseGithub" role="button" aria-expanded="false" aria-controls="collapseGithub">
+            <h3 class="cor_texto change-font-size">
+              Github 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M4.854 14.854a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V3.5A2.5 2.5 0 0 1 6.5 1h8a.5.5 0 0 1 0 1h-8A1.5 1.5 0 0 0 5 3.5v9.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4z"/>
+              </svg>
+            </h3>
+          </a>
+          <div class="collapse" id="collapseGithub">
+              <p class="change-font-size">Alex Teixeira - <a class="text-primary" href=""><u>GitHub</u></a> </p>
+              <p class="change-font-size">Caio Oliveira - <a class="text-primary" href=""><u>GitHub</u></a></p>
+              <p class="change-font-size">Guilherme Moreira - <a class="text-primary" href=""><u>GitHub</u></a></p>
+              <p class="change-font-size">Leonardo Bozzi - <a class="text-primary" href=""><u>GitHub</u></a> </p>
+              <p class="change-font-size">Juarez Souza - <a class="text-primary" href=""><u>GitHub</u></a> </p>
+          </div>
         </div>
       </div>
       <hr>
       <div class="row mt-3">
         <div class="col">
-          <h3 style="color:#8844EE" class="change-font-size">Instagram</h3>
+          <a data-bs-toggle="collapse" href="#collapseInstagram" role="button" aria-expanded="false" aria-controls="collapseInstagram">
+            <h3 class="cor_texto change-font-size">
+              Instagram 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M4.854 14.854a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V3.5A2.5 2.5 0 0 1 6.5 1h8a.5.5 0 0 1 0 1h-8A1.5 1.5 0 0 0 5 3.5v9.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4z"/>
+              </svg>
+            </h3>
+          </a>
+          <div class="collapse" id="collapseInstagram">
+              <p class="change-font-size">Alex Teixeira - <a class="text-primary" href=""><u>Instagram</u></a> </p>
+              <p class="change-font-size">Caio Oliveira - <a class="text-primary" href=""><u>Instagram</u></a></p>
+              <p class="change-font-size">Guilherme Moreira - <a class="text-primary" href=""><u>Instagram</u></a></p>
+              <p class="change-font-size">Leonardo Bozzi - <a class="text-primary" href=""><u>Instagram</u></a> </p>
+              <p class="change-font-size">Juarez Souza - <a class="text-primary" href=""><u>Instagram</u></a> </p>
+          </div>
         </div>
       </div>
       <hr class="mb-5">
